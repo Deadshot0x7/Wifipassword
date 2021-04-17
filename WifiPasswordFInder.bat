21cf4659ae1a02851f87e3eba86d154ebe977364
DeadShot0x7 <68690437+Deadshot0x7@users.noreply.github.com>
2021-04-18 03:07:20 +0530

    Add files via upload

---
+++ WifiPasswordFInder.bat
@@ -0,0 +1,13 @@
+cls
+color 0a
+@ECHO %RANDOM% %RANDOM% %RANDOM% %RANDOM% %RANDOM% %RANDOM%
+
+@echo off
+Title Window's Wifi PAssword Finder 
+@Echo  Created By Deadshot0x7
+ NETSH WLAN SHOW PROFILE 
+SET wifiname=Varad
+NETSH WLAN SHOW PROFILE %wifiname% Key=clear
+@ECHO %RANDOM% %RANDOM% %RANDOM% %RANDOM% %RANDOM% %RANDOM%
+
+pause>nul
